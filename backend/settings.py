1e991c9788f76298b4e59bfac657854ebd79aa89
Gerlin Gonzalez <sadonfa@gmail.com>
2025-09-17 22:21:51 -0500

    Update Cors Headers

--- backend/settings.py
+++ backend/settings.py
@@ -127,8 +127,8 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
-    'https://api.gegova.com.co'
-    "https://curriculum.gegova.com.co"
+    'https://api.gegova.com.co',
+    'https://curriculum.gegova.com.co'
 ]
 
 # Password validation
